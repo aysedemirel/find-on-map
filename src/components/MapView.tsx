c2c548ae84c191badf67ccf0d8499957fbdf8462
Ayşe Demirel Deniz <aysedemirel1996@gmail.com>
2025-08-29 17:39:35 +0500

    Fix marker visibility issue

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -31,7 +31,20 @@ const MapView: React.FC<MapPickerProps> = ({
     return (
       <>
         {firstPos && (
-          <Marker position={firstPos} icon={new L.Icon.Default()}>
+          <Marker
+            position={firstPos}
+            icon={
+              new L.Icon({
+                iconUrl:
+                  'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png',
+                shadowUrl:
+                  'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+                iconSize: [25, 41],
+                iconAnchor: [12, 41],
+                popupAnchor: [1, -34],
+                shadowSize: [41, 41]
+              })
+            }>
             <Popup>First coordinate</Popup>
           </Marker>
         )}
